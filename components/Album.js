0e7870b903a252ea6faee40f97cabb4191a4da0b
Blendi <blendidedushi@gmail.com>
2023-08-18 21:25:38 +0200

    Search Songs added

--- components/Album.js
+++ components/Album.js
@@ -1,30 +1,14 @@
 export default function Album(album){
     return `
-    <div id="carouselExampleIndicators" class="carousel slide">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <div class = "container d-flex flex-column">
+      <h1 class="text-center text-dark font-monospace fw-bold">${album.full_title}</h1>
+      <h2>Description</h2>
+      <p>${album.description_preview}</p>
+      <div class="d-flex justify-content-between">
+          <p class="fw-bold"><i class="bi bi-calendar-week"></i> ${album.release_date}</p>
+      </div>
+      <img src=${album.header_image_url}>
+   
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
     `
 }
---
+++ components/SearchedSongs.js
@@ -0,0 +1,30 @@
+export default function SearchedSongs(songs){
+    let songs_html= `<div class="row">`
+    
+    songs.forEach(song =>{
+        songs_html += `
+        <div class="card border-dark-subtle mb-4 container-fluid chover bg-body-tertiary">
+                <div class="row">
+                    <div class="col-md-2">
+                        <img src="${song.result.header_image_thumbnail_url}" class="img-thumbnail mt-1" style="max-width: 150px;" alt="...">
+                    </div>
+                    <div class="col-md-9">
+                        <div class="card-body">
+                            <h5 class="card-title"><i class="bi bi-music-note"></i> ${song.result.full_title}</h5>
+                            <p class="card-text text-decoration-underline">${song.result.artist_names}</p>
+                            <p class="card-text mt-5"><small class="text-body-secondary"><i class="bi bi-calendar3"></i> ${song.result.release_date_for_display} </small></p>
+                        </div>
+                    </div>
+                    <div class="col-md-1 my-auto">
+                        <a href="song.html?id=${song.result.id}" class="ms-3 btn btn-outline-dark"><i class="bi bi-caret-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+    })
+
+    songs_html += `</div>`
+
+    return songs_html
+}
